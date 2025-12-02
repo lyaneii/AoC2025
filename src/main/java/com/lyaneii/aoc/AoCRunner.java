38043f334bae5cd966ff5dd4b3cdcd1dafe60463
lyaneii <146889907+lyaneii@users.noreply.github.com>
2025-12-02 23:24:20 +0100

    refactor: move aoc running functions to own class

---
+++ src/main/java/com/lyaneii/aoc/AoCRunner.java
@@ -0,0 +1,72 @@
+package com.lyaneii.aoc;
+
+import com.lyaneii.aoc.common.Day;
+import com.lyaneii.aoc.common.http.AocHttpClient;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
+
+public class AoCRunner {
+    private final int currentDay;
+    private static final int AOC_LOCAL_START_HOUR = 6;
+    private static final String DAY_CLASS_PREFIX = "com.lyaneii.aoc.days.Day";
+    private final AocHttpClient httpClient;
+
+    public AoCRunner() {
+        currentDay = getCurrentAvailableDay();
+        httpClient = new AocHttpClient();
+    }
+
+    public int getCurrentAvailableDay() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        int day = localDateTime.getDayOfMonth();
+
+        if (localDateTime.getHour() < AOC_LOCAL_START_HOUR) {
+            return day - 1;
+        }
+        return day;
+    }
+
+    public void runCurrentDay() {
+        try {
+            runForSpecifiedDay(currentDay);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void runAllDays() {
+        for (int dayNumber = 1; dayNumber <= currentDay; ++dayNumber) {
+            try {
+                runForSpecifiedDay(dayNumber);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void runForSpecifiedDay(int dayNumber)
+            throws ClassNotFoundException, NoSuchMethodException,
+            InvocationTargetException, InstantiationException, IllegalAccessException {
+        if (dayNumber > currentDay) {
+            return;
+        }
+
+        Day day = (Day) Class.forName(DAY_CLASS_PREFIX + dayNumber).getDeclaredConstructor().newInstance();
+
+        httpClient.createExampleInput(day);
+        httpClient.createInput(day);
+
+        day.printTitle();
+
+        day.parseExampleInput();
+        System.out.println("Example: ");
+        System.out.println("Part One: " + day.partOne());
+        System.out.println("Part Two: " + day.partTwo());
+
+        day.parseInput();
+        System.out.println("To Submit: ");
+        System.out.println("Part One: " + day.partOne());
+        System.out.println("Part Two: " + day.partTwo());
+    }
+}
--- src/main/java/com/lyaneii/aoc/Main.java
+++ src/main/java/com/lyaneii/aoc/Main.java
@@ -1,35 +1,12 @@
 package com.lyaneii.aoc;
 
-import com.lyaneii.aoc.common.Day;
-import com.lyaneii.aoc.common.http.AocHttpClient;
-
-import java.lang.reflect.InvocationTargetException;
+import com.lyaneii.aoc.common.DayTemplateGenerator;
 
 public class Main {
-    private static final int CURRENT_DAY = 2;
+    public static void main(String[] args) {
+        AoCRunner runner = new AoCRunner();
 
-    public static void main(String[] args) throws ClassNotFoundException,
-            NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-
-        AocHttpClient client = new AocHttpClient();
-
-        for (int dayNumber = 1; dayNumber <= CURRENT_DAY; ++dayNumber) {
-            Day day = (Day) Class.forName("com.lyaneii.aoc.days.Day" + dayNumber).getDeclaredConstructor().newInstance();
-
-            client.createExampleInput(day);
-            client.createInput(day);
-
-            day.printTitle();
-
-            day.parseExampleInput();
-            System.out.println("Example: ");
-            System.out.println("Part One: " + day.partOne());
-            System.out.println("Part Two: " + day.partTwo());
-
-            day.parseInput();
-            System.out.println("To Submit: ");
-            System.out.println("Part One: " + day.partOne());
-            System.out.println("Part Two: " + day.partTwo());
-        }
+        DayTemplateGenerator.generate(runner.getCurrentAvailableDay());
+        runner.runCurrentDay();
     }
 }
