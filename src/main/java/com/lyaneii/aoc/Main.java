63bf600c9ec2b1d89ca39a789499aad872c5c2da
lyaneii <146889907+lyaneii@users.noreply.github.com>
2025-12-04 22:28:56 +0100

    chore: change default state of main function

--- src/main/java/com/lyaneii/aoc/Main.java
+++ src/main/java/com/lyaneii/aoc/Main.java
@@ -1,12 +1,8 @@
 package com.lyaneii.aoc;
 
-import com.lyaneii.aoc.common.DayTemplateGenerator;
-
 public class Main {
     public static void main(String[] args) {
         AoCRunner runner = new AoCRunner();
-
-        DayTemplateGenerator.generate(runner.getCurrentAvailableDay());
-        runner.runCurrentDay(AoCRunner.RUN_WITH_INPUT);
+        runner.runAllDays();
     }
 }
